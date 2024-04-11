a477c7f0f738ca1e42fcd8251ae5a37cd7a00600
JeremyBaumiller <jbaumil07@gmail.com>
2024-04-10 23:04:14 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="card">
+        
+        <p>
+     
+        </p>
+      </div>
+      <p className="read-the-docs">
+    
+      </p>
+    </>
+  );
+}
+
+export default App;
